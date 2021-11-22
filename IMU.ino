c95f81643a7163887ed9e850cc1e6e51b9ed1e4e
MaxT098 <92232042+MaxT098@users.noreply.github.com>
2021-11-22 16:24:00 +0000

    Add files via upload

---
+++ IMU.ino
@@ -0,0 +1,117 @@
+#include <Wire.h>
+#include <LSM6.h>
+#include <Motors.h>
+#include <LineSensors.h>
+#include <Others.h>
+
+#define LINE_SENSOR_UPDATE  100
+#define MOTOR_UPDATE        2000
+#define MOTOR_RUNTIME       500
+#define MOTOR_PWR           40
+#define TURNING_PWR         500
+
+#define LINE_THRESHOLD      2000
+#define DF_TIMEOUT          3000
+#define ELINE_THRESHOLD     0.2
+
+// LSM6 is a class.  We create an instance
+// of this class called "imu".  We will then
+// use imu to access the device by method
+// functions and variables inside the class.
+LSM6 imu;
+
+float accel;
+float gyro;
+
+void setup() {
+
+  // Start the wire library for i2c.
+  // Note: do not add this command into
+  // a class constructor. It must occur
+  // (or be called) from setup().
+  Wire.begin();
+
+  // Serial for debug output
+  Serial.begin(9600);
+  Serial.println("***RESET***");
+  delay(1000);
+
+  // Check the IMU initialised ok.
+  if (!imu.init() ) {  // no..? :(
+
+    // Since we failed to communicate with the
+    // IMU, we put the robot into an infinite
+    // while loop and report the error.
+    while (1) {
+      Serial.println("Failed to detect and initialize IMU!");
+      delay(1000);
+    }
+  }
+
+  // IMU initialise ok!
+  // Set the IMU with default settings.
+  imu.enableDefault();
+
+} // End of setup()
+
+// Simple loop to report all the readings from
+// the accelerometer and gyro
+// imu.a = accelerometer
+// imu.g = gyro
+void loop() {
+
+  // Make a read of the sensor.
+  imu.read();
+
+  // Report values
+  accel = (imu.a.y * 0.061 / 1000);
+  gyro = (imu.g.z * 8.75 / 1000);
+
+  if (gyro < 0) {
+    accel = -(accel);
+  }
+
+  lineFollowingBehaviour();
+
+  // Short delay to keep things
+  // slow enough to observe.
+  // There is a limit to how fast you
+  // can make i2c readings.
+  delay(100);
+
+
+} // End of loop()
+
+void lineFollowingBehaviour() {
+  unsigned long ls_ts;
+  unsigned long motor_ts;
+  unsigned long elapsed_time;
+  float Eline;
+
+  elapsed_time = current_ts - ls_ts;
+  if ( elapsed_time > LINE_SENSOR_UPDATE ) {
+    LineSensors.AllLineSensorsRead();
+    ls_ts = millis();
+  }
+
+  elapsed_time = current_ts - motor_ts;
+  if ( elapsed_time > MOTOR_UPDATE ) {
+    Eline = LineSensors.getLineError();
+
+    //When not detecting a line
+    if (LineSensors.elapsed_t[0] > LINE_THRESHOLD && LineSensors.elapsed_t[1] > LINE_THRESHOLD && LineSensors.elapsed_t[2] > LINE_THRESHOLD) {
+      Motors.setLeftMotorPower(-TURNING_PWR * gyro);
+      Motors.setRightMotorPower(TURNING_PWR * gyro);
+    } else if (Eline > ELINE_THRESHOLD) {
+      Motors.setLeftMotorPower(-TURNING_PWR * Eline);
+      Motors.setRightMotorPower(TURNING_PWR * Eline);
+    } else if (Eline < -ELINE_THRESHOLD) {
+      Motors.setLeftMotorPower(-TURNING_PWR * Eline);
+      Motors.setRightMotorPower(TURNING_PWR * Eline);
+    } else {
+      Motors.setLeftMotorPower(MOTOR_PWR);
+      Motors.setRightMotorPower(MOTOR_PWR);
+    }
+    motor_ts = millis();
+  }
+}
---
+++ LineSensors.h
@@ -0,0 +1,103 @@
+#ifndef _LINESENSORS_H
+#define _LINESENSORS_H
+
+class LineSensors {
+  public:
+#define EMIT_PIN 11
+#define LS_LEFT_IN_PIN 18
+#define LS_CENTRE_IN_PIN 20
+#define LS_RIGHT_IN_PIN 21
+#define timeout 2500
+#define NB_LS_PINS 3
+
+    int elapsed_t[NB_LS_PINS] = {};
+
+    LineSensors () {
+
+    }
+
+    void initialise() {
+      pinMode(EMIT_PIN, OUTPUT);
+      pinMode(LS_LEFT_IN_PIN, INPUT);
+      pinMode(LS_CENTRE_IN_PIN, INPUT);
+      pinMode(LS_RIGHT_IN_PIN, INPUT);
+      digitalWrite(EMIT_PIN, HIGH);
+    }
+
+    void AllLineSensorsRead() {
+      unsigned long start_time;
+      unsigned long end_time;
+      unsigned long d_t;
+      bool done = false;
+
+      int ls_pin[NB_LS_PINS] = { LS_LEFT_IN_PIN, LS_CENTRE_IN_PIN, LS_RIGHT_IN_PIN };
+      unsigned long end_t[NB_LS_PINS] = {};
+
+      pinMode(LS_LEFT_IN_PIN, OUTPUT);
+      pinMode(LS_CENTRE_IN_PIN, OUTPUT);
+      pinMode(LS_RIGHT_IN_PIN, OUTPUT);
+      digitalWrite(LS_LEFT_IN_PIN, HIGH);
+      digitalWrite(LS_CENTRE_IN_PIN, HIGH);
+      digitalWrite(LS_RIGHT_IN_PIN, HIGH);
+      delayMicroseconds(10);
+      pinMode(LS_LEFT_IN_PIN, INPUT);
+      pinMode(LS_CENTRE_IN_PIN, INPUT);
+      pinMode(LS_RIGHT_IN_PIN, INPUT);
+      start_time = micros();
+
+      while (done == false) {
+        int i;
+        for (i = 0; i < NB_LS_PINS; i++) {
+          if (digitalRead(ls_pin[i]) == LOW && end_t[i] == 0) {
+            end_t[i] = micros();
+          }
+        }
+        if (digitalRead(ls_pin[0]) == digitalRead(ls_pin[1]) == digitalRead(ls_pin[2]) == LOW && end_t[0] != 0 && end_t[1] != 0 && end_t[2] != 0) {
+          for (i = 0; i < NB_LS_PINS; i++) {
+            if (elapsed_t[i] <= timeout) {
+              elapsed_t[i] = end_t[i] - start_time;
+            } else {
+              elapsed_t[i] = timeout;
+            }
+          }
+          done = true;
+        }
+      }
+    }
+
+    float getLineError() {
+      float Lraw;
+      float Craw;
+      float Rraw;
+      float Lreading;
+      float Creading;
+      float Rreading;
+      float Lnorm;
+      float Cnorm;
+      float Rnorm;
+      float Lweight;
+      float Rweight;
+      float Eline;
+
+      Lreading = elapsed_t[0];
+      Creading = elapsed_t[1];
+      Rreading = elapsed_t[2];
+
+      //      Serial.print(Lreading); Serial.print(" ");
+      //      Serial.print(Creading); Serial.print(" ");
+      //      Serial.print(Rreading); Serial.print(" ");
+      //      Serial.println();
+
+      Lnorm = Lreading / (Lreading + Creading + Rreading);
+      Cnorm = Creading / (Lreading + Creading + Rreading);
+      Rnorm = Rreading / (Lreading + Creading + Rreading);
+
+      Lweight = Lnorm + (Cnorm * 0.25);
+      Rweight = Rnorm + (Cnorm * 0.25);
+      Eline = Lweight - Rweight;
+
+      return Eline;
+    }
+};
+
+#endif
